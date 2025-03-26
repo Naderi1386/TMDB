d1e5b66ce5173d5eaaea5c5d1025adf0aafea519
unknown <mh.naderi1386@gmail.com>
2025-03-26 16:29:56 +0330

    create a global layout and add metadata and font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,20 @@
 import { ReactNode } from "react";
 import "./globals.css";
+import { Metadata } from "next";
+
+import { Inter } from "next/font/google";
+const inter = Inter({
+  display: "swap",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: {
+    default: "TMDB",
+    template: "%s TMDB",
+  },
+  description: "The TMDB website is here for you to find the best ones",
+};
 
 interface RootLayoutPropsType {
   children: ReactNode;
@@ -8,7 +23,7 @@ interface RootLayoutPropsType {
 export default function RootLayout({ children }: RootLayoutPropsType) {
   return (
     <html lang="en">
-      <body className={` antialiased`}>{children}</body>
+      <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
   );
 }
