8ef73d89a535ec77637d83123e0ecc9525469a37
unknown <mh.naderi1386@gmail.com>
2025-03-27 17:34:18 +0330

    add a simple footer

--- app/_components/Footer.tsx
+++ app/_components/Footer.tsx
@@ -1,13 +1,23 @@
 import FooterImage from "./FooterImage";
+import FooterList from "./FooterList";
 import FooterUserName from "./FooterUserName";
 
 const Footer = () => {
   return (
     <footer>
-      <div className="bg-blue-950 px-[20rem] pt-11 pb-20">
+      <div className="bg-blue-950 px-[20rem] pt-11 pb-20 flex gap-12 ">
         <div className="space-y-12">
           <FooterImage />
           <FooterUserName />
+        </div>
+        <div>
+          <FooterList
+            title="The Basics"
+            items={[
+              { text: "About TMDB", href: "/about" },
+              
+            ]}
+          />
         </div>
       </div>
     </footer>
---
+++ app/_components/FooterList.tsx
@@ -0,0 +1,28 @@
+import Link from "next/link";
+
+interface FooterListItemsType {
+  text: string;
+  href: string;
+}
+
+interface FooterListPropsType {
+  title: string;
+  items: FooterListItemsType[];
+}
+
+const FooterList = ({ title, items }: FooterListPropsType) => {
+  return (
+    <div className="text-white">
+      <h2 className="font-bold mb-1 uppercase">{title}</h2>
+      <ul className="space-y-1">
+        {items.map((item, index) => (
+          <li key={index} className="transition-all duration-150 hover:scale-105">
+            <Link href={item.href}>{item.text}</Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default FooterList;
