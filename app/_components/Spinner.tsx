44b4366c4817a16af948c45c316c019c94a94fc0
unknown <mh.naderi1386@gmail.com>
2025-03-28 16:38:22 +0330

    new loading spinner

--- app/_components/Spinner.tsx
+++ app/_components/Spinner.tsx
@@ -2,7 +2,7 @@
 const Spinner = () => {
   return (
     <div className="flex justify-center items-center pt-[8rem]">
-      <div className="w-16 h-16 border-4 border-blue-950 border-t-transparent rounded-full animate-spin"></div>
+      <div className="w-20 h-18 border-8 border-blue-950 border-t-transparent rounded-full animate-spin"></div>
     </div>
   );
 }
