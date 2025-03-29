8079d79d741a315d1f3a67737c97340303b2efc0
unknown <mh.naderi1386@gmail.com>
2025-03-29 18:17:45 +0330

    change ui

--- app/_components/MoviesItem.tsx
+++ app/_components/MoviesItem.tsx
@@ -1,18 +1,35 @@
 import Image from "next/image";
-import { MovieType } from "../_lib/services"
+import { MovieType } from "../_lib/services";
+import { formatDate } from "../_lib/helper";
 
-interface MoviesItemPropsType{
-    movie:MovieType
+interface MoviesItemPropsType {
+  movie: MovieType;
 }
-const MoviesItem = ({movie}: MoviesItemPropsType) => {
-    const {title,poster_path}=movie
-    const src = `https://image.tmdb.org/t/p/w500${poster_path}`;
-   
-  return <li className="basis-[17.5%] rounded-md">
-    <div className="w-full relative h-[280px]">
-        <Image fill className="object-cover" alt={`Image-${title}`} src={src} unoptimized  />
-    </div>
-  </li>;
+const MoviesItem = ({ movie }: MoviesItemPropsType) => {
+  const { title, poster_path,release_date } = movie;
+  const src = `https://image.tmdb.org/t/p/w500${poster_path}`;
+  const date = formatDate(release_date);
+
+  return (
+    <li className="basis-[17.5%] rounded-md border border-solid border-stone-300 overflow-hidden ">
+      <div className="w-full relative h-[250px] border-b border-solid border-stone-300   rounded-t-md cursor-pointer">
+        <Image
+        quality={80}
+          fill
+          className="object-cover "
+          alt={`Image-${title}`}
+          src={src}
+          unoptimized
+        />
+      </div>
+      <div className="px-3 py-8 ">
+        <h3 className="text-sm font-bold transition-all duration-150 hover:text-sky-400 cursor-pointer">
+          {title}
+        </h3>
+        <span className="text-sm text-stone-400">{date}</span>
+      </div>
+    </li>
+  );
 };
 
-export default MoviesItem
+export default MoviesItem;
--- app/_components/MoviesList.tsx
+++ app/_components/MoviesList.tsx
@@ -7,7 +7,7 @@ interface MoviesListPropsType {
 
 const MoviesList = async ({ type }: MoviesListPropsType) => {
   const movies = await getMoviesByType(type);
-  return <ul className="flex flex-wrap justify-between items-center gap-6">
+  return <ul className="flex flex-wrap justify-between items-stretch gap-6">
     {movies.map((movie)=><MoviesItem movie={movie} key={movie.id} />)}
   </ul>;
 };
