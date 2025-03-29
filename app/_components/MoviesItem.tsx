9142570d6e0f523c1a4a51ad0720fcdff983d194
unknown <mh.naderi1386@gmail.com>
2025-03-29 18:25:16 +0330

    finish movie items for now

--- app/_components/MoviesItem.tsx
+++ app/_components/MoviesItem.tsx
@@ -1,32 +1,39 @@
 import Image from "next/image";
 import { MovieType } from "../_lib/services";
 import { formatDate } from "../_lib/helper";
+import Link from "next/link";
 
 interface MoviesItemPropsType {
   movie: MovieType;
 }
 const MoviesItem = ({ movie }: MoviesItemPropsType) => {
-  const { title, poster_path,release_date } = movie;
+  const { title, poster_path,release_date,vote_average,id } = movie;
   const src = `https://image.tmdb.org/t/p/w500${poster_path}`;
   const date = formatDate(release_date);
 
   return (
     <li className="basis-[17.5%] rounded-md border border-solid border-stone-300 overflow-hidden ">
-      <div className="w-full relative h-[250px] border-b border-solid border-stone-300   rounded-t-md cursor-pointer">
-        <Image
-        quality={80}
-          fill
-          className="object-cover "
-          alt={`Image-${title}`}
-          src={src}
-          unoptimized
-        />
-      </div>
-      <div className="px-3 py-8 ">
-        <h3 className="text-sm font-bold transition-all duration-150 hover:text-sky-400 cursor-pointer">
+      <Link href={`movies/${id}`}>
+        <div className="w-full relative h-[250px] border-b border-solid border-stone-300   rounded-t-md cursor-pointer">
+          <Image
+            quality={80}
+            fill
+            className="object-cover "
+            alt={`Image-${title}`}
+            src={src}
+            unoptimized
+          />
+        </div>
+      </Link>
+      <div className="px-3 py-4 ">
+        <Link
+          href={`movies/${id}`}
+          className="text-sm font-bold transition-all duration-150 hover:text-sky-400 cursor-pointer"
+        >
           {title}
-        </h3>
-        <span className="text-sm text-stone-400">{date}</span>
+        </Link>
+        <span className="text-sm text-stone-400 block">{date}</span>
+        <span className="text-xs font-bold">{vote_average} ⭐</span>
       </div>
     </li>
   );
