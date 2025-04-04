a8f5c0de226012cf58809309af3bb62f038860d8
unknown <mh.naderi1386@gmail.com>
2025-04-04 19:48:48 +0330

    add a simple img for erroring the movies

--- app/_components/TrendMovieItem.tsx
+++ app/_components/TrendMovieItem.tsx
@@ -1,7 +1,9 @@
+"use client";
 import Link from "next/link";
 import { formatDate } from "../_lib/helper";
 import { MovieType } from "../_lib/services";
 import Image from "next/image";
+import { useState } from "react";
 
 interface TrendMovieItemPropsType {
   movie: MovieType;
@@ -9,17 +11,21 @@ interface TrendMovieItemPropsType {
 const TrendMovieItem = ({ movie }: TrendMovieItemPropsType) => {
   const { title, poster_path, release_date, vote_average, id } = movie;
   const src = `https://image.tmdb.org/t/p/w500${poster_path}`;
+  const [img, setImg] = useState(src);
   const date = formatDate(release_date);
   return (
     <li title={title} className="min-w-[150px]  rounded-md  overflow-hidden ">
       <Link href={`movies/${id}`}>
         <div className="w-full relative h-[250px]    cursor-pointer">
           <Image
-            quality={80}
+            quality={90}
             fill
             className="object-cover rounded-md"
             alt={`Image-${title}`}
-            src={src}
+            src={img}
+            onError={() =>
+              setImg("https://demofree.sirv.com/nope-not-here.jpg?w=120")
+            }
             unoptimized
           />
         </div>
