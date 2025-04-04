c39059956ca383f50576b00a8293aeea52249d4a
unknown <mh.naderi1386@gmail.com>
2025-04-04 19:34:44 +0330

    change ui

--- app/_components/TrendingList.tsx
+++ app/_components/TrendingList.tsx
@@ -12,7 +12,7 @@ const TrendingList = ({ movies }: TrandingListPropsType) => {
         <h2>Trending</h2>
       </div>
       <div className="overflow-auto bg-center bg-no-repeat bg-cover bg-[url(https://www.themoviedb.org/assets/2/v4/misc/trending-bg-39afc2a5f77e31d469b25c187814c0a2efef225494c038098d62317d923f8415.svg)]">
-        <ul className="flex items-stretch justify-between gap-4 ">
+        <ul className="flex items-stretch justify-between gap-6 ">
           {movies.map((movie) => (
             <TrendMovieItem movie={movie} key={movie.id} />
           ))}
