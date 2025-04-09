5702717a6a277a3eafb559ba229f95f84ad72d68
unknown <mh.naderi1386@gmail.com>
2025-04-09 14:31:19 +0330

    add some new informations to details

--- app/_components/DetailsSmallInformations.tsx
+++ app/_components/DetailsSmallInformations.tsx
@@ -1,9 +1,46 @@
-
-
-const DetailsSmallInformations = () => {
-  return (
-    <div>DetailsSmallInformations</div>
-  )
+interface DetailsSmallInformationsPropsType {
+  release_date: string;
+  genres: [
+    {
+      id: number;
+      name: string;
+    },
+    {
+      id: number;
+      name: string;
+    }
+  ];
+  runtime: number;
 }
 
-export default DetailsSmallInformations
+const DetailsSmallInformations = ({
+  release_date,
+  runtime,
+  genres,
+}: DetailsSmallInformationsPropsType) => {
+  const houre = runtime / 60;
+  const minuet = runtime % 60;
+  
+  const date=release_date.split("-").reverse().join("/")
+  return (
+    <div className="flex items-center gap-3">
+      <span className="block">{date}</span>
+      <div className="bg-white w-[5px] h-[5px] rounded-[100%]"></div>
+      <div className="flex items-center gap-1">
+        {genres.map((genre, index) => (
+          <span key={genre.id}>
+            {genre.name}
+            {index !== genres.length - 1 && ","}
+          </span>
+        ))}
+      </div>
+      <div className="bg-white w-[5px] h-[5px] rounded-[100%]"></div>
+      <div className="flex items-center gap-1">
+        <span>{Math.trunc(houre)}h</span>
+        <span>{minuet}m</span>
+      </div>
+    </div>
+  );
+};
+
+export default DetailsSmallInformations;
--- app/_components/MovieDetails.tsx
+++ app/_components/MovieDetails.tsx
@@ -6,11 +6,11 @@ interface MovieDetailsPropsType{
     details:MovieDetailsType
 }
 const MovieDetails = ({details}: MovieDetailsPropsType) => {
-    const {poster_path,title,release_date}=details
+    const {poster_path,title,release_date,genres,runtime}=details
     const src = `https://media.themoviedb.org/t/p/w600_and_h900_bestv2${poster_path}`;
   return (
     <div>
-      <div className="bg-red-500 px-[10rem] py-8 flex items-start gap-10 ">
+      <div className="bg-sky-700 px-[10rem] py-8 flex items-start gap-10 ">
         <div className="w-[22.5%] relative h-[400px] border border-solid border-white rounded-md overflow-hidden ">
           <DetailsPoster src={src} title={title} />
         </div>
@@ -20,7 +20,7 @@ const MovieDetails = ({details}: MovieDetailsPropsType) => {
                 <span className="font-light text-4xl text-gray-200">({release_date.slice(0,4)})</span>
 
             </div>
-            <DetailsSmallInformations />
+            <DetailsSmallInformations runtime={runtime} genres={genres} release_date={release_date} />
         </div>
       </div>
     </div>
