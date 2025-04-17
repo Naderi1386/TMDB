978850b8582d23a5ba4837661a4263cb41f14e89
unknown <mh.naderi1386@gmail.com>
2025-04-17 16:21:37 +0330

    add a new middleware

---
+++ middleware.ts
@@ -0,0 +1,16 @@
+import { NextRequest, NextResponse } from "next/server";
+import { auth } from "./auth";
+
+const protectedRoutes = ["/tv", "/movies"];
+
+const middleware = async (request: NextRequest) => {
+  const session = await auth();
+  const { pathname } = request.nextUrl;
+  const isProtected = protectedRoutes.includes(pathname);
+  if (!session && isProtected) {
+    return NextResponse.redirect(new URL("/login", request.url));
+  }
+  return NextResponse.next();
+};
+
+export default middleware;
