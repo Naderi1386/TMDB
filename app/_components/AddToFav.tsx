6c36290a2090da0251f52799a59a0e2bc3b2dbc3
unknown <mh.naderi1386@gmail.com>
2025-04-18 20:33:13 +0330

    add a button for removeing&adding favorites

--- app/_components/AddToFav.tsx
+++ app/_components/AddToFav.tsx
@@ -6,13 +6,19 @@ interface AddToFavPropsType {
   item: FavoriteType;
 }
 const AddToFav = ({ item }: AddToFavPropsType) => {
-  const {  addFav } = useFavoritesStore();
+  const { addFav, removeFav, favorites } = useFavoritesStore();
+
+  const isAdded = favorites.map((fav) => fav.id).includes(item.id);
   return (
     <button
-      onClick={() => addFav(item)}
+      title={isAdded ? "Remove from favorites list" : "Add to favorites list"}
+      onClick={() => {
+        if (!isAdded) addFav(item);
+        else removeFav(item.id);
+      }}
       className="w-[2.8rem] h-[2.8rem] flex items-center justify-center rounded-[100%] bg-blue-950 cursor-pointer"
     >
-      <FaHeart size={13} color="white" />
+      <FaHeart size={13} color={isAdded ? "red" : "white"} />
     </button>
   );
 };
