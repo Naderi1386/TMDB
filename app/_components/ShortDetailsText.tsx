1ab3fa1df412bd7cf0a88fc535ed0e1cd7933e22
unknown <mh.naderi1386@gmail.com>
2025-04-18 17:11:38 +0330

    add a regex function

--- app/_components/ShortDetailsText.tsx
+++ app/_components/ShortDetailsText.tsx
@@ -1,13 +1,27 @@
-interface ShortDetailsTextPropsType{
-    title:string
-    value:string | number
+function SplitFromRightRegex(val: string) {
+  const reversed = val.split("").reverse().join("");
+  const parts = reversed.match(/.{1,3}/g);
+  const result = parts
+    ?.map((val) => val.split("").reverse().join(""))
+    .reverse()
+    .join(",");
+  return result;
 }
-const ShortDetailsText = ({title,value}: ShortDetailsTextPropsType) => {
-  return <div className="flex flex-col gap-2 text-black text-center">
-    <span className="text-lg font-bold">{title}</span>
-    <span className="font-light">{value}</span>
 
-  </div>;
+interface ShortDetailsTextPropsType {
+  title: string;
+  value: string | number;
+}
+const ShortDetailsText = ({ title, value }: ShortDetailsTextPropsType) => {
+  const isNumber = typeof value === "number";
+  const specialVal = SplitFromRightRegex(value.toString());
+
+  return (
+    <div className="flex flex-col gap-2 text-black text-center">
+      <span className="text-lg font-bold">{title}</span>
+      <span className="font-light">{isNumber ? `$${specialVal}` : value}</span>
+    </div>
+  );
 };
 
-export default ShortDetailsText
+export default ShortDetailsText;
