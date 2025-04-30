ca9b1cdd48db9fece0812e8b4cb756bd7edbb41c
unknown <mh.naderi1386@gmail.com>
2025-04-30 16:31:32 +0330

    change codes

--- app/_lib/actions.ts
+++ app/_lib/actions.ts
@@ -5,7 +5,6 @@ import { signIn, signOut } from "@/auth";
 export const login = async () => {
   await signIn("github", { redirectTo: "/" });
 };
-
 export const logout = async () => {
   await signOut({ redirectTo: "/" });
 };
