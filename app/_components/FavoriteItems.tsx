805037c1ec25800b7cf93e21ef0623b8e091019c
unknown <mh.naderi1386@gmail.com>
2025-05-01 17:46:59 +0330

    change ui and create favorites items and its details

--- app/_components/FavoriteItems.tsx
+++ app/_components/FavoriteItems.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import { FavoriteType } from "../_store/FavoritesStore";
+import { formatDate } from "../_lib/helper";
 
 interface FavoriteItemsPropsType {
   favorite: FavoriteType;
@@ -7,14 +8,38 @@ interface FavoriteItemsPropsType {
 }
 const FavoriteItems = ({ favorite, index }: FavoriteItemsPropsType) => {
   const { date, id, rating, revenue, runtime, title, type } = favorite;
-  return <li className="flex items-center gap-4">
-    <span className="text-gray-400">{index + 1}</span>
-    <div className="grow border border-solid border-black/8 px-3 py-4 rounded-sm flex items-center justify-between">
-        <Link className="text-black text-sm font-bold transition-all duration-150 hover:underline" href={type==="movie" ? `/movies/${id}` : `/tv/${id}`}>{title}</Link>
+  const revenueVal=String(revenue).slice(0,3)
+  return (
+    <li className="flex items-center gap-4">
+      <span className="text-gray-400">{index + 1}</span>
+      <div className="grow border border-solid border-black/8 px-3 py-4 rounded-sm flex items-center justify-between">
+        <Link
+          className="text-black text-sm font-bold transition-all duration-150 hover:underline"
+          href={type === "movie" ? `/movies/${id}` : `/tv/${id}`}
+        >
+          {title}
+        </Link>
+        <div className="flex items-center justify-end gap-4">
+          <span className="text-gray-400 text-[14px]">{rating} ⭐</span>
+          <span className="text-white rounded-3xl text-xs py-[6px] px-3 bg-gradient-to-r from-amber-300 to-rose-500 capitalize">
+            {type}
+          </span>
+          <span className="bg-slate-950 text-white text-xs py-[6px] px-3 rounded-3xl">
+            {formatDate(date)}
+          </span>
+          <div className="flex items-center gap-[1px] text-gray-400 text-xs">
+            <span>{(runtime/60).toFixed(0)}h</span>
+            <span>{runtime%60}m</span>
 
-    </div>
+          </div>
+          <span className="text-xs text-gray-400">
+            ${revenueVal}M
 
-  </li>;
+          </span>
+        </div>
+      </div>
+    </li>
+  );
 };
 
 export default FavoriteItems;
