b9af6ac87d32e010ddf59e64fd683287e68dfb4c
unknown <mh.naderi1386@gmail.com>
2025-05-04 21:51:14 +0330

    add a searchParams for sorting favorites

--- app/_components/FavoritesSort.tsx
+++ app/_components/FavoritesSort.tsx
@@ -1,18 +1,17 @@
-"use client"
+"use client";
 
 import { usePathname, useRouter, useSearchParams } from "next/navigation";
 
 const FavoritesSort = () => {
-    const pathname=usePathname()
-    const {replace}=useRouter()
-    const searchParams=useSearchParams()
+  const pathname = usePathname();
+  const { replace } = useRouter();
+  const searchParams = useSearchParams();
 
-    const handleClick=(val:string)=>{
-        const params=new URLSearchParams(searchParams)
-        params.set("sort",val)
-        replace(`${pathname}?${params}`)
-
-    }
+  const handleClick = (val: string) => {
+    const params = new URLSearchParams(searchParams);
+    params.set("sort", val);
+    replace(`${pathname}?${params}`);
+  };
   return (
     <div className="flex justify-center">
       <div className="dropdown dropdown-center dropdown-hover mt-2 ">
@@ -37,6 +36,6 @@ const FavoritesSort = () => {
       </div>
     </div>
   );
-}
+};
 
-export default FavoritesSort
+export default FavoritesSort;
--- app/favorites/page.tsx
+++ app/favorites/page.tsx
@@ -4,11 +4,19 @@ import FavoritesTitleSection from "../_components/FavoritesTitleSection";
 import FavoritesList from "../_components/FavoritesList";
 import FavoritesSort from "../_components/FavoritesSort";
 
-export const metadata:Metadata={
-     title:'Favorites'
+export const metadata: Metadata = {
+  title: "Favorites",
+};
+interface SearchParamsType {
+  sort: string;
 }
 
-const page = () => {
+interface PagePropsType {
+  searchParams: Promise<SearchParamsType>;
+}
+
+const page = async ({ searchParams }: PagePropsType) => {
+  const sort = await (await searchParams).sort || "all";
   return (
     <section>
       <div>
