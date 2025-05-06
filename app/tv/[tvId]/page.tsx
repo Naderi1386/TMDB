c751abfda0351116ab12051c00cde655beee3233
unknown <mh.naderi1386@gmail.com>
2025-05-06 17:11:29 +0330

    add some components to tv details page

--- app/tv/[tvId]/page.tsx
+++ app/tv/[tvId]/page.tsx
@@ -1,10 +1,17 @@
+import PopularContent from "@/app/_components/PopularContent";
+import TrendContent from "@/app/_components/TrendContent";
+import TrendingList from "@/app/_components/TrendingList";
 import { getDetailsTVShow } from "@/app/_lib/services";
 
 interface ParamsType {
   tvId: string;
 }
+interface SearchParamsType{
+    time?:string
+}
 interface PagePropsType {
   params: Promise<ParamsType>;
+  searchParams:Promise<SearchParamsType>
 }
 export const generateMetadata = async (props: PagePropsType) => {
   const tvId = (await props.params).tvId;
@@ -16,9 +23,20 @@ export const generateMetadata = async (props: PagePropsType) => {
 
 const page = async (props: PagePropsType) => {
   const tvId = await (await props.params).tvId;
+  const tvShowDetails=await getDetailsTVShow(tvId)
+  const time=await (await props.searchParams).time || "day"
   return (
     <div>
-      
+      <div className="mt-12 px-[10rem] ">
+        <div>
+          <TrendContent>
+            <TrendingList time={time} />
+          </TrendContent>
+        </div>
+        <div className="mt-12">
+          <PopularContent />
+        </div>
+      </div>
     </div>
   );
 };
