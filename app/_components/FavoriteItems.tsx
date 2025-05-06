2354c1ae27feeeee01e3e7683fb7c1f0ec41a431
unknown <mh.naderi1386@gmail.com>
2025-05-06 17:40:02 +0330

    add some changes to favorites items

--- app/_components/FavoriteItems.tsx
+++ app/_components/FavoriteItems.tsx
@@ -9,7 +9,7 @@ interface FavoriteItemsPropsType {
 }
 const FavoriteItems = ({ favorite, index }: FavoriteItemsPropsType) => {
   const { date, id, rating, revenue, runtime, title, type } = favorite;
-  const revenueVal=String(revenue).slice(0,3)
+  const revenueVal=revenue && String(revenue).slice(0,3)
   return (
     <li className="flex items-center gap-4">
       <span className="text-gray-400">{index + 1}</span>
@@ -21,22 +21,23 @@ const FavoriteItems = ({ favorite, index }: FavoriteItemsPropsType) => {
           {title}
         </Link>
         <div className="flex items-center justify-end gap-4">
-          <span className="text-gray-400 text-[14px]">{rating }⭐</span>
+          <span className="text-gray-400 text-[14px]">{rating}⭐</span>
           <span className="text-white rounded-3xl text-xs py-[6px] px-3 bg-gradient-to-r from-amber-300 to-rose-500 capitalize">
             {type}
           </span>
           <span className="bg-slate-950 text-white text-xs py-[6px] px-3 rounded-3xl">
             {formatDate(date)}
           </span>
-          <div className="flex items-center gap-[1px] text-gray-400 text-xs">
-            <span>{(runtime/60).toFixed(0)}h</span>
-            <span>{runtime%60}m</span>
+          {runtime && (
+            <div className="flex items-center gap-[1px] text-gray-400 text-xs">
+              <span>{(runtime / 60).toFixed(0)}h</span>
+              <span>{runtime % 60}m</span>
+            </div>
+          )}
+          {revenue && (
+            <span className="text-xs text-gray-400">${revenueVal}M</span>
+          )}
 
-          </div>
-          <span className="text-xs text-gray-400">
-            ${revenueVal}M
-
-          </span>
           <FavoriteDeleteBtn id={id} />
         </div>
       </div>
