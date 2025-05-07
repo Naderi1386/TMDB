d33855a1429964b448195f41cee166ed61907cb9
unknown <mh.naderi1386@gmail.com>
2025-05-07 18:28:14 +0330

    get page and query with promise.all

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,8 +1,17 @@
-
-const page = () => {
-  return (
-    <div>page</div>
-  )
+interface SearchParamsType {
+  page: string;
+  query: string;
+}
+interface PagePropsType {
+  searchParams: Promise<SearchParamsType>;
 }
 
-export default page
+const page = async ({ searchParams }: PagePropsType) => {
+  const [page, query] = await Promise.all([
+    (await searchParams).page || "1",
+    (await searchParams).query,
+  ]);
+  return <div>page</div>;
+};
+
+export default page;
