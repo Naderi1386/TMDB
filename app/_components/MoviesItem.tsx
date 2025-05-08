2496f75805cd5ea03f7a1a4d661cc361d7332d2a
unknown <mh.naderi1386@gmail.com>
2025-05-08 14:58:49 +0330

    working on default poster p1

--- app/_components/MoviesItem.tsx
+++ app/_components/MoviesItem.tsx
@@ -11,7 +11,7 @@ interface MoviesItemPropsType {
 const MoviesItem = ({ movie }: MoviesItemPropsType) => {
   const { title, poster_path, release_date, vote_average, id } = movie;
   const src = `https://image.tmdb.org/t/p/w500${poster_path}`;
-  const [img, setImg] = useState("/images/no-image.jpg");
+  const [img, setImg] = useState(src);
   const date = formatDate(release_date);
 
   return (
@@ -20,16 +20,14 @@ const MoviesItem = ({ movie }: MoviesItemPropsType) => {
       className="basis-[17.5%] rounded-md border border-solid border-stone-300 overflow-hidden "
     >
       <Link href={`movies/${id}`}>
-        <div className="w-full relative h-[250px] border-b border-solid border-stone-300   rounded-t-md cursor-pointer">
+        <div className="w-full relative h-[250px] border-b border-solid border-stone-300 bg-black/20  rounded-t-md cursor-pointer">
           <Image
             quality={80}
             fill
             className="object-cover "
             alt={`Image-${title}`}
             onError={() => setImg("/images/no-image.jpg")}
-            onLoadingComplete={() => {
-              setImg(src);
-            }}
+           
             src={img}
             unoptimized
           />
@@ -43,7 +41,9 @@ const MoviesItem = ({ movie }: MoviesItemPropsType) => {
           {title}
         </Link>
         <span className="text-sm text-stone-400 block">{date}</span>
-        <span className="text-xs font-bold text-black">{vote_average.toFixed(1)} ⭐</span>
+        <span className="text-xs font-bold text-black">
+          {vote_average.toFixed(1)} ⭐
+        </span>
       </div>
     </li>
   );
