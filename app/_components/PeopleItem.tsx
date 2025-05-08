6df0e990dcdee321aaa53af6196cf6f342212a70
unknown <mh.naderi1386@gmail.com>
2025-05-08 16:39:08 +0330

    change ui in people item and fix some problems

--- app/_components/PeopleItem.tsx
+++ app/_components/PeopleItem.tsx
@@ -6,13 +6,19 @@ interface PeopleItemPropsType {
   peopleItem: PeopleType;
 }
 const PeopleItem = ({ peopleItem }: PeopleItemPropsType) => {
-  const { original_name, profile_path, known_for,id } = peopleItem;
+  const { original_name, profile_path, known_for, id, known_for_department } =
+    peopleItem;
   const src = `https://media.themoviedb.org/t/p/w470_and_h470_face${profile_path}`;
   return (
     <li className="w-[23.5%]">
       <PeopleProfile src={src} />
-      <div className="bg-white border border-solid border-stone-300 shadow-xl p-3">
-        <Link href={`/people/${id}`} className="text-black font-bold transition-all duration-150 hover:text-blue-500">{original_name}</Link>
+      <div className="bg-white h-[9rem] border border-solid border-stone-300 shadow-xl px-3 py-2 overflow-auto">
+        <Link
+          href={`/people/${id}`}
+          className="text-black font-bold transition-all duration-150 hover:text-blue-500"
+        >
+          {original_name}
+        </Link>
         <ul className="flex items-center flex-wrap">
           {known_for.map((item, index) => (
             <li key={index}>
@@ -23,6 +29,9 @@ const PeopleItem = ({ peopleItem }: PeopleItemPropsType) => {
             </li>
           ))}
         </ul>
+        <span className="text-xs font-bold text-black">
+          {known_for_department}
+        </span>
       </div>
     </li>
   );
