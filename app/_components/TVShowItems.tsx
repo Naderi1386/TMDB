2b79139b0898771b501f84f3cb920976dc421b0c
unknown <mh.naderi1386@gmail.com>
2025-05-08 14:59:32 +0330

    working on default poster p2

--- app/_components/TVShowItems.tsx
+++ app/_components/TVShowItems.tsx
@@ -11,7 +11,7 @@ interface TVShowItemsPropsType{
 const TVShowItems = ({show}: TVShowItemsPropsType) => {
     const {id,first_air_date,poster_path,name,vote_average}=show
   const src = `https://media.themoviedb.org/t/p/w500${poster_path}`;
-  const [img, setImg] = useState("/images/no-image.jpg");
+  const [img, setImg] = useState(src);
   const date = formatDate(first_air_date);
 
   return (
@@ -20,7 +20,7 @@ const TVShowItems = ({show}: TVShowItemsPropsType) => {
       className="basis-[17.5%] rounded-md border border-solid border-stone-300 overflow-hidden "
     >
       <Link href={`tv/${id}`}>
-        <div className="w-full relative h-[250px] border-b border-solid border-stone-300   rounded-t-md cursor-pointer">
+        <div className="w-full relative h-[250px] bg-black/20 border-b border-solid border-stone-300   rounded-t-md cursor-pointer">
           <Image
             quality={80}
             fill
@@ -28,9 +28,7 @@ const TVShowItems = ({show}: TVShowItemsPropsType) => {
             alt={`Image-${name}`}
             src={img}
             onError={() => setImg("/images/no-image.jpg")}
-            onLoadingComplete={() => {
-              setImg(src);
-            }}
+       
             unoptimized
           />
         </div>
@@ -43,7 +41,9 @@ const TVShowItems = ({show}: TVShowItemsPropsType) => {
           {name}
         </Link>
         <span className="text-sm text-stone-400 block">{date}</span>
-        <span className="text-xs font-bold text-black">{vote_average.toFixed(1)} ⭐</span>
+        <span className="text-xs font-bold text-black">
+          {vote_average.toFixed(1)} ⭐
+        </span>
       </div>
     </li>
   );
