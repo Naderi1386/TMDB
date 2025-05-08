88e9c4b11e1ed4a97acfa66f0ff92cdc8f938835
unknown <mh.naderi1386@gmail.com>
2025-05-08 17:24:01 +0330

    change ui in emptyContent

--- app/_components/EmptyContent.tsx
+++ app/_components/EmptyContent.tsx
@@ -9,7 +9,7 @@ interface EmptyContentPropsType {
 const EmptyContent = ({ children,topic }: EmptyContentPropsType) => {
 
   return (
-    <div className="text-center font-bold text-blue-950 pt-12">
+    <div className="text-center font-bold text-blue-950 py-12">
       <h2 className="text-2xl mb-6">{children}</h2>
       <DeleteSearchParamsBtn topic={topic} />
     </div>
