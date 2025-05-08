82299bff58a1f7d3160e858eb8119fbb06a338dd
unknown <mh.naderi1386@gmail.com>
2025-05-08 13:09:23 +0330

    add a function to get people

--- app/_lib/services.ts
+++ app/_lib/services.ts
@@ -372,3 +372,22 @@ export const searchMulti = async (query: string, page: string) => {
     throw new Error("TV show details could not be loaded !");
   }
 };
+
+export const getPopularPeoples = async (page: string) => {
+  const url = `https://api.themoviedb.org/3/person/popular?language=en-US&page=${page}`;
+  const options = {
+    method: "GET",
+    headers: {
+      accept: "application/json",
+      Authorization: `Bearer ${process.env.API_TOKEN as string}`,
+    },
+  };
+  try {
+    const request = await fetch(url, options);
+    const response = await request.json();
+    return response
+  } catch (error) {
+    console.error(error);
+    throw new Error("People could not be loaded !");
+  }
+};
