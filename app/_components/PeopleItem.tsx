7409b9f78747a9a027267c3265a0549fe4c0529d
unknown <mh.naderi1386@gmail.com>
2025-05-08 13:45:54 +0330

    add a people profile component

--- app/_components/PeopleItem.tsx
+++ app/_components/PeopleItem.tsx
@@ -1,4 +1,5 @@
 import { PeopleType } from "../_lib/services";
+import PeopleProfile from "./PeopleProfile";
 
 interface PeopleItemPropsType {
   peopleItem: PeopleType;
@@ -6,8 +7,8 @@ interface PeopleItemPropsType {
 const PeopleItem = ({ peopleItem }: PeopleItemPropsType) => {
     const {gender,id,known_for_department,name,original_name,popularity,profile_path}=peopleItem
     const src = `https://media.themoviedb.org/t/p/w470_and_h470_face${profile_path}`;
-  return <li>
-    
+  return <li className="w-[25%]">
+    <PeopleProfile src={src} />
   </li>;
 };
 
---
+++ app/_components/PeopleProfile.tsx
@@ -0,0 +1,8 @@
+interface PeopleProfilePropsType {
+  src: string;
+}
+const PeopleProfile = ({ src }: PeopleProfilePropsType) => {
+  return <div>PeopleProfile</div>;
+};
+
+export default PeopleProfile;
