d9c12ff25d1bf38ee1e5c3773f0397ec8362f9ae
unknown <mh.naderi1386@gmail.com>
2025-05-08 14:56:18 +0330

    add a default no person img when we get error in peopel item

--- app/_components/PeopleProfile.tsx
+++ app/_components/PeopleProfile.tsx
@@ -1,12 +1,22 @@
+"use client";
 import Image from "next/image";
+import { useState } from "react";
 
 interface PeopleProfilePropsType {
   src: string;
 }
 const PeopleProfile = ({ src }: PeopleProfilePropsType) => {
+  const [img, setImg] = useState(src);
+
   return (
-    <div className="relative w-full h-[300px] bg-black rounded-[7px] overflow-hidden cursor-pointer">
-      <Image fill className="object-cover" src={src} alt="Person-Profile" />
+    <div className="relative w-full h-[300px] bg-black/20 rounded-[7px] overflow-hidden cursor-pointer">
+      <Image
+        onError={() => setImg("/images/no-person.png")}
+        fill
+        className="object-cover"
+        src={img}
+        alt="Person-Profile"
+      />
     </div>
   );
 };
