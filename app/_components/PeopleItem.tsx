e0b08fcae37a318c87334509da2f41a454b3f4c0
unknown <mh.naderi1386@gmail.com>
2025-05-08 13:42:51 +0330

    create src for person profile

--- app/_components/PeopleItem.tsx
+++ app/_components/PeopleItem.tsx
@@ -1,10 +1,14 @@
-import { PeopleType } from "../_lib/services"
+import { PeopleType } from "../_lib/services";
 
-interface PeopleItemPropsType{
-    peopleItem:PeopleType
+interface PeopleItemPropsType {
+  peopleItem: PeopleType;
 }
-const PeopleItem = ({peopleItem}: PeopleItemPropsType) => {
-  return <li>PeopleItem</li>;
+const PeopleItem = ({ peopleItem }: PeopleItemPropsType) => {
+    const {gender,id,known_for_department,name,original_name,popularity,profile_path}=peopleItem
+    const src = `https://media.themoviedb.org/t/p/w470_and_h470_face${profile_path}`;
+  return <li>
+    
+  </li>;
 };
 
-export default PeopleItem
+export default PeopleItem;
