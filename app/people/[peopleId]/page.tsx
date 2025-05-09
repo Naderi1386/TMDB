e36cef0e6755fe27602038e7ad4f8948ad6ebbd1
unknown <mh.naderi1386@gmail.com>
2025-05-09 15:51:45 +0330

    add a metadata for showing persons names in title

--- app/people/[peopleId]/page.tsx
+++ app/people/[peopleId]/page.tsx
@@ -1,8 +1,18 @@
+import { getPersonDetails } from "@/app/_lib/services";
+
 interface ParamsType {
   peopleId: string;
 }
 interface PagePropsType {
   params: Promise<ParamsType>;
+}
+export async function generateMetadata(props:PagePropsType){
+    const peopleId=(await props.params).peopleId
+    const {name}=await getPersonDetails(peopleId)
+    return {
+        title:`${name.toLocaleUpperCase()}-Details`
+    }
+
 }
 
 const page = ({ params }: PagePropsType) => {
