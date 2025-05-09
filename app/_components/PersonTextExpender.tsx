c88b7b0269d66510194446f7f0f126441181f973
unknown <mh.naderi1386@gmail.com>
2025-05-09 16:52:29 +0330

    add a btn to show all the content in person details

--- app/_components/PersonTextExpender.tsx
+++ app/_components/PersonTextExpender.tsx
@@ -7,8 +7,20 @@ interface PersonTextExpenderPropsType {
 }
 const PersonTextExpender = ({ text }: PersonTextExpenderPropsType) => {
   const [isShow, setIsShow] = useState(false);
-  const shortText=text.split("")
-  return <></>;
+  const shortText = text.split(" ").slice(0, 25).join(" ");
+  return (
+    <>
+      {isShow ? text : `${shortText}...`}
+      {!isShow && (
+        <button
+          onClick={() => setIsShow(true)}
+          className="btn btn-xs btn-dash btn-info"
+        >
+          Show all
+        </button>
+      )}
+    </>
+  );
 };
 
 export default PersonTextExpender;
