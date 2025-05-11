8bc40c1afdaa88e1589ee65ab8ffa3c96ef0a7ca
unknown <mh.naderi1386@gmail.com>
2025-05-11 16:02:23 +0330

    add toaster

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,15 +8,16 @@ import { Shrikhand } from "next/font/google";
 import Header from "./_components/Header";
 import Footer from "./_components/Footer";
 import ScrollButton from "./_components/ScrollButton";
+import { Toaster } from "react-hot-toast";
 const inter = Inter({
   display: "swap",
   subsets: ["latin"],
 });
-export const shrikhand=Shrikhand({
-  display:"swap",
-  subsets:['latin'],
-  weight:"400"
-})
+export const shrikhand = Shrikhand({
+  display: "swap",
+  subsets: ["latin"],
+  weight: "400",
+});
 
 export const metadata: Metadata = {
   title: {
@@ -34,11 +35,26 @@ interface RootLayoutPropsType {
 export default function RootLayout({ children }: RootLayoutPropsType) {
   return (
     <html lang="en">
-      <body className={`${inter.className} antialiased flex flex-col h-dvh overflow-x-hidden`}>
+      <body
+        className={`${inter.className} antialiased flex flex-col h-dvh overflow-x-hidden`}
+      >
         <Header />
         <main className="grow bg-white">{children}</main>
         <Footer />
         <ScrollButton />
+        <Toaster
+          position="top-center"
+          reverseOrder={false}
+          gutter={8}
+          containerClassName=""
+          containerStyle={{}}
+          toastOptions={{
+            className: "",
+            duration: 2500,
+            removeDelay: 1000,
+           
+          }}
+        />
       </body>
     </html>
   );
