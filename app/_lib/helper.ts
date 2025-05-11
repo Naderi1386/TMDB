f5e4b30c047025bd45d1606cd1c3ec346ff6b05e
unknown <mh.naderi1386@gmail.com>
2025-05-11 14:20:26 +0330

    calculateAge function

--- app/_lib/helper.ts
+++ app/_lib/helper.ts
@@ -6,3 +6,38 @@ export function formatDate(dateStr: string): string {
     day: "numeric",
   });
 }
+
+export function calculateAge(birthDate: string) {
+  const today = new Date();
+  const birth = new Date(birthDate);
+
+  if (isNaN(birth.getTime())) {
+    return "تاریخ وارد شده معتبر نیست";
+  }
+
+  let years: number = today.getFullYear() - birth.getFullYear();
+
+  const hasBirthdayPassed =
+    today.getMonth() > birth.getMonth() ||
+    (today.getMonth() === birth.getMonth() &&
+      today.getDate() >= birth.getDate());
+
+  if (!hasBirthdayPassed) {
+    years--;
+  }
+
+  const lastBirthday = new Date(
+    hasBirthdayPassed ? today.getFullYear() : today.getFullYear() - 1,
+    birth.getMonth(),
+    birth.getDate()
+  );
+
+  const days = Math.floor(
+    (Number(today) - Number(lastBirthday)) / (1000 * 60 * 60 * 24)
+  );
+
+  return {
+    years: years,
+    days: days,
+  };
+}
