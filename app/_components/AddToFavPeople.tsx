971bd85e373d8882f1bb5e24f34d60e790f2bb6f
unknown <mh.naderi1386@gmail.com>
2025-07-12 23:18:53 +0330

    added AddToFavPeople & RemoveFavPeople functions to AddToFavPeople component

--- app/_components/AddToFavPeople.tsx
+++ app/_components/AddToFavPeople.tsx
@@ -1,11 +1,26 @@
+"use client";
 import { FaHeart } from "react-icons/fa";
 import { PersoneDetailsType } from "../_lib/services";
+import { useFavoritesStore } from "../_store/FavoritesStore";
 
 const AddToFavPeople = ({ persone }: { persone: PersoneDetailsType }) => {
-    
+  const { favoritesPeople, addFavPeople, removeFavPeople } =
+    useFavoritesStore();
+  const isAdded = favoritesPeople.find((fav) => fav.id === persone.id);
   return (
-    <button className="w-[2.8rem] h-[2.8rem] flex items-center justify-center rounded-[100%] bg-blue-950 cursor-pointer">
-      <FaHeart size={13} color={"white"} />
+    <button
+      title={
+        isAdded
+          ? "Remove from favorites people list"
+          : "Add to favorites people list"
+      }
+      onClick={() => {
+        if (!isAdded) addFavPeople(persone);
+        else removeFavPeople(String(persone.id));
+      }}
+      className="w-[2.8rem] h-[2.8rem] flex items-center justify-center rounded-[100%] bg-blue-950 cursor-pointer"
+    >
+      <FaHeart size={13} color={isAdded ? "red" : "white"} />
     </button>
   );
 };
